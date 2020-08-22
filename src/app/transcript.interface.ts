d96250a01e7143369267f60d24723c3ba5ed207a
System Administrator <bmulhern09@gmail.com>
2020-08-22 13:42:45 -0400

    Deploy

---
+++ src/app/transcript.interface.ts
@@ -0,0 +1,12 @@
+/*
+Title: Assignment 7.3
+Author: Professor Krasso
+Date: 22 August 2020
+Modified By: Brendan Mulhern
+Description: This is project is the GPA-Calulator App!
+*/
+
+export interface iTranscript {
+    course: string;
+    grade: string;
+}
